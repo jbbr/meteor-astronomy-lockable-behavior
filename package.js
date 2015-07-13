28e2e9bff3e5a7fbadc7d9ce4a20b0681caf8a99
Janek Thomaschewski <janek@jbbr.net>
2015-07-13 14:51:44 +0200

    Switch to explicit semicolon style

--- package.js
+++ package.js
@@ -4,15 +4,15 @@ Package.describe({
   summary: 'Lockable behavior for Meteor Astronomy',
   git: 'https://github.com/jbbr/meteor-astronomy-lockable-behavior',
   documentation: 'README.md'
-})
+});
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2')
+  api.versionsFrom('1.1.0.2');
 
-  api.use('jagi:astronomy-behaviors@0.6.0')
-  api.use('underscore')
+  api.use('jagi:astronomy-behaviors@0.6.0');
+  api.use('underscore');
 
-  api.addFiles('lib/behavior/methods.js')
-  api.addFiles('lib/behavior/events.js')
-  api.addFiles('lib/behavior/behavior.js')
-})
+  api.addFiles('lib/behavior/methods.js');
+  api.addFiles('lib/behavior/events.js');
+  api.addFiles('lib/behavior/behavior.js');
+});
